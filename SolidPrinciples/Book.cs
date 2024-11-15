f08a55ac106254f0590588f49e56061a590dd654
TEKFA Fouad <fouad.tekfa@etu.univ-lehavre.fr>
2024-11-15 16:28:20 +0100

    add calss Book

---
+++ SolidPrinciples/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples
+{
+    public class Book
+    {
+        public string Titel { get; set; } = "";
+        public string Author { get; set; } = "";
+        public int NbPages { get; set; }
+        public string ISBN { get; set; } = "";
+
+        public async Task SaveToFile()
+        {
+            await File.WriteAllTextAsync($"./book-{Titel}.json", JsonSerializer.Serialize(this,new
+                JsonSerializerOptions
+            { 
+            PropertyNameCaseInsensitive = true,
+            }
+                ));
+        }
+    }
+}
